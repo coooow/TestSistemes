3d6caf98f3380e366887be982407b0fac53d8b6c
cow <marxfox@gmail.com>
2025-03-31 11:21:38 +0200

    mega update

--- scripts.js
+++ scripts.js
@@ -12,6 +12,8 @@ var tema6 = [];
 var tema6copy = [];
 var tema7 = [];
 var tema7copy = [];
+var temaAll = [];
+var temaAllcopy = [];
 
 let currentQuestion;
 var selectedTema;
@@ -26,6 +28,74 @@ function nextQuestion() {
     unselect();
     firstAnswer = true;
 
+    let qTitle = document.getElementById("questionText");
+
+    if (selectedTema === 1) {
+        if (tema1copy.length === 0) {
+            showFinished();
+            return;
+        }
+        let n = Math.floor(Math.random() * tema1copy.length);
+        currentQuestion = tema1copy.splice(n, 1)[0];
+    } else if (selectedTema === 2) {
+        if (tema2copy.length === 0) {
+            showFinished();
+            return;
+        }
+        let n = Math.floor(Math.random() * tema2copy.length);
+        currentQuestion = tema2copy.splice(n, 1)[0];
+    } else if (selectedTema === 3) {
+        if (tema3copy.length === 0) {
+            showFinished();
+            return;
+        }
+        let n = Math.floor(Math.random() * tema3copy.length);
+        currentQuestion = tema3copy.splice(n, 1)[0];
+    } else if (selectedTema === 4) {
+        if (tema4copy.length === 0) {
+            showFinished();
+            return;
+        }
+        let n = Math.floor(Math.random() * tema4copy.length);
+        currentQuestion = tema4copy.splice(n, 1)[0];
+    } else if (selectedTema === 5) {
+        if (tema5copy.length === 0) {
+            showFinished();
+            return;
+        }
+        let n = Math.floor(Math.random() * tema5copy.length);
+        currentQuestion = tema5copy.splice(n, 1)[0];
+    } else if (selectedTema === 6) {
+        if (tema6copy.length === 0) {
+            showFinished();
+            return;
+        }
+        let n = Math.floor(Math.random() * tema6copy.length);
+        currentQuestion = tema6copy.splice(n, 1)[0];
+    } else if (selectedTema === 7) {
+        if (tema7copy.length === 0) {
+            showFinished();
+            return;
+        }
+        let n = Math.floor(Math.random() * tema7copy.length);
+        currentQuestion = tema7copy.splice(n, 1)[0];
+    } else if (selectedTema === 8) {
+        if (temaAllcopy.length === 0) {
+            showFinished();
+            return;
+        }
+
+        let n = Math.floor(Math.random() * temaAllcopy.length);
+        currentQuestion = temaAllcopy.splice(n, 1)[0];
+    }
+
+    qTitle.innerHTML = currentQuestion.question;
+    for (let i = 1; i < 5; i++) {
+        let answer = currentQuestion.answers[i - 1].answer;
+        let a = document.getElementById("label" + i);
+        a.innerHTML = answer;
+    }
+
     var count = document.getElementById("counterText");
     var c = count.innerHTML.split("/");
     var correct = parseInt(c[0]);
@@ -33,81 +103,22 @@ function nextQuestion() {
     total++;
     count.innerHTML = correct + "/" + total;
 
-    let qTitle = document.getElementById("questionText");
-
-    if(selectedTema === 1) {
-        if(tema1copy.length === 0) {
-            qTitle.innerHTML = "No questions available for Tema " + selectedTema;
-            return;
-        }
-        let n = Math.floor(Math.random() * tema1copy.length);
-        currentQuestion = tema1copy.splice(n, 1)[0];
-    } else if(selectedTema === 2) {
-        if(tema2copy.length === 0) {
-            qTitle.innerHTML = "No questions available for Tema " + selectedTema;
-            return;
-        }
-        let n = Math.floor(Math.random() * tema2copy.length);
-        currentQuestion = tema2copy.splice(n, 1)[0];
-    } else if(selectedTema === 3) {
-        if(tema3copy.length === 0) {
-            qTitle.innerHTML = "No questions available for Tema " + selectedTema;
-            return;
-        }
-        let n = Math.floor(Math.random() * tema3copy.length);
-        currentQuestion = tema3copy.splice(n, 1)[0];
-    } else if(selectedTema === 4) {
-        if(tema4copy.length === 0) {
-            qTitle.innerHTML = "No questions available for Tema " + selectedTema;
-            return;
-        }
-        let n = Math.floor(Math.random() * tema4copy.length);
-        currentQuestion = tema4copy.splice(n, 1)[0];
-    } else if(selectedTema === 5) {
-        if(tema5copy.length === 0) {
-            qTitle.innerHTML = "No questions available for Tema " +selectedTema;
-            return;
-        }
-        let n = Math.floor(Math.random() * tema5copy.length);
-        currentQuestion = tema5copy.splice(n, 1)[0];
-    } else if(selectedTema === 6) {
-        if(tema6copy.length === 0) {
-            qTitle.innerHTML = "No questions available for Tema " + selectedTema;
-            return;
-        }
-        let n = Math.floor(Math.random() * tema6copy.length);
-        currentQuestion = tema6copy.splice(n, 1)[0];
-    } else if(selectedTema === 7) {
-        if(tema7copy.length === 0) {
-            qTitle.innerHTML = "No questions available for Tema " + selectedTema;
-            return;
-        }
-        let n = Math.floor(Math.random() * tema7copy.length);
-        currentQuestion = tema7copy.splice(n, 1)[0];
-    }
-
-    qTitle.innerHTML = currentQuestion.question;
-    for(let i = 1; i < 5; i++) {
-        let answer = currentQuestion.answers[i-1].answer; 
-        let a = document.getElementById("label" + i);
-        a.innerHTML = answer;
-    }
-
     answered = false;
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     resetCounter();
     fetch("questions.json")
         .then(response => response.json()) // Parse JSON response
         .then(data => {
-            tema1 = data.tema1 || []; 
-            tema2 = data.tema2 || []; 
-            tema3 = data.tema3 || []; 
-            tema4 = data.tema4 || []; 
-            tema5 = data.tema5 || []; 
-            tema6 = data.tema6 || []; 
-            tema7 = data.tema7 || []; 
+            tema1 = data.tema1 || [];
+            tema2 = data.tema2 || [];
+            tema3 = data.tema3 || [];
+            tema4 = data.tema4 || [];
+            tema5 = data.tema5 || [];
+            tema6 = data.tema6 || [];
+            tema7 = data.tema7 || [];
+            temaAll = tema1.concat(tema2, tema3, tema4, tema5, tema6, tema7);
         })
         .catch(error => console.error("Error loading JSON:", error));
 });
@@ -117,11 +128,11 @@ function answer() {
     document.getElementById("correct").style.display = "none";
     document.getElementById("wrong").style.display = "none";
 
-    for(let i = 0; i < 4; i++) {
-        if(currentQuestion.answers[i].answer === selectedAnswer) {
-            if(currentQuestion.answers[i].correct) {
+    for (let i = 0; i < 4; i++) {
+        if (currentQuestion.answers[i].answer === selectedAnswer) {
+            if (currentQuestion.answers[i].correct) {
                 document.getElementById("correct").style.display = "block";
-                if(firstAnswer){
+                if (firstAnswer) {
                     var count = document.getElementById("counterText");
                     var c = count.innerHTML.split("/");
                     var correct = parseInt(c[0]);
@@ -135,7 +146,7 @@ function answer() {
         }
     }
 
-    if(firstAnswer){
+    if (firstAnswer) {
         firstAnswer = false;
     }
 
@@ -146,17 +157,17 @@ function unselect() {
     document.getElementById("correct").style.display = "none";
     document.getElementById("wrong").style.display = "none";
     let answers = document.getElementsByName("answer");
-    for(let i = 0; i < 4; i++) {
+    for (let i = 0; i < 4; i++) {
         answers[i].checked = false;
     }
 }
 
-function resetCounter(){
+function resetCounter() {
     var count = document.getElementById("counterText");
     count.innerHTML = "0/0";
 }
 
-function resetQuestions(){
+function resetQuestions() {
     tema1copy = tema1.slice();
     tema2copy = tema2.slice();
     tema3copy = tema3.slice();
@@ -164,40 +175,58 @@ function resetQuestions(){
     tema5copy = tema5.slice();
     tema6copy = tema6.slice();
     tema7copy = tema7.slice();
+    temaAllcopy = temaAll.slice();
 }
 
-function selectTema(tema){
+function selectTema(tema) {
     selectedTema = tema;
     document.getElementById("testMenu").style.display = "block";
     document.getElementById("menu").style.display = "none";
     resetQuestions();
     nextQuestion();
-    answered = false;  
+    answered = false;
 }
 
-function tornarMenu(){
+function tornarMenu() {
     document.getElementById("testMenu").style.display = "none";
+    document.getElementById("finished").style.display = "none";
+    document.getElementById("popupBg").style.display = "none";
     document.getElementById("menu").style.display = "block";
     resetCounter();
     answered = true;
 }
 
-function isChecked(){
+function isChecked() {
     let selectedAnswer = null;
 
-    if(document.getElementById("testMenu").style.display != "block"){
+    if (document.getElementById("testMenu").style.display != "block") {
         return;
     }
 
-    for(let i = 1; i < 5; i++) {
+    for (let i = 1; i < 5; i++) {
         let answer = document.getElementById("answer" + i);
-        if(answer.checked) {
+        if (answer.checked) {
             selectedAnswer = document.getElementById("label" + i).innerHTML;
             break;
         }
     }
-    if(selectedAnswer === null) {
+    if (selectedAnswer === null) {
         alert("Please select an answer!");
     }
     return selectedAnswer;
+}
+
+function showFinished() {
+    document.getElementById("finished").style.display = "block";
+    document.getElementById("popupBg").style.display = "block";
+}
+
+function retry() {
+    document.getElementById("finished").style.display = "none";
+    document.getElementById("popupBg").style.display = "none";
+    resetQuestions();
+    resetCounter();
+    nextQuestion();
+    answered = false;
+    firstAnswer = true;
 }
